ef0063208c23abe0bb5913d8ebba3116d29e02e9
Asani Sathsarani <asangisathsarani@gmail.com>
2019-03-22 00:44:15 +0530

    Frontend-oginform

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router , Route } from 'react-router-dom';
 import Main from './component/Main';
 import Customer from './customer/Customer';
-// import Login from './component/pages/login';
+import Login from './supplier/Login';
 
 class App extends Component {
   render() {
@@ -11,8 +11,8 @@ class App extends Component {
             <div className="cover-container d-flex h-100  mx-auto flex-column">
               <Route path="/" exact component={Main}/>
               <Route path="/customer" exact component={Customer}/>
-            {/* <Route path="/login" component={Login}/>
-            <Route path="/create" component={Create}/> */}
+              <Route path="/supplier/login" exact component={Login}/>
+
           </div>
         </Router>
     );
--- src/component/pages/customer.register.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-class CustomerRegister extends Component{
-    render(){
-        return{
-            
-        }
-    }
-}
